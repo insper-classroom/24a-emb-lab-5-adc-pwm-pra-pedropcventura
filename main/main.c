9d17ddab6b8988ee53d71e0e73f8fbe846545a6f
pedropcventura <pedropventura@icloud.com>
2024-04-08 15:15:30 -0300

    primeira versao

--- main/main.c
+++ main/main.c
@@ -20,20 +20,80 @@ typedef struct adc {
     int val;
 } adc_t;
 
-
 void uart_task(void *p) {
     adc_t data;
+    int msb;
+    int lsb;
 
     while (1) {
-        xQueueReceive(xQueueAdc, &data, portMAX_DELAY);
+        if (xQueueReceive(xQueueAdc, &data, portMAX_DELAY) == pdTRUE) {
+            msb = data.val >> 8;
+            lsb = data.val & 0xFF;
+            uart_putc_raw(uart0, data.axis);
+            uart_putc_raw(uart0, msb);
+            uart_putc_raw(uart0, lsb);
+            uart_putc_raw(uart0, -1);
+            //printf("valor: %d\n", data.val);
+        }
     }
 }
 
+
+void x_task(void *p) {
+    adc_init();
+    adc_gpio_init(26); 
+    adc_t data;
+
+    int nt;
+    while (1) {
+        adc_select_input(0); 
+        data.axis = 0;
+
+        nt = adc_read();
+        nt -= 2048;
+        nt = nt/8;
+        if (nt>-100&&nt<100){
+            nt = 0;
+        }
+        data.val = nt;
+
+        xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(100)); 
+    }
+}
+
+void y_task(void *p) {
+    adc_init();
+    adc_gpio_init(27); 
+    adc_t data;
+
+    int nt;
+    while (1) {
+        adc_select_input(1); 
+        data.axis = 1;
+
+        nt = adc_read();
+        nt -= 2048;
+        nt = 255*nt/2047;
+        if (nt>-100&&nt<100){
+            nt = 0;
+        }
+        data.val = nt;
+
+        xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(100)); 
+    }
+}
+
+
 int main() {
     stdio_init_all();
+    adc_init();
 
     xQueueAdc = xQueueCreate(32, sizeof(adc_t));
 
+    xTaskCreate(x_task, "XAxisTask", 4096, NULL, 1, NULL);
+    xTaskCreate(y_task, "Y Axis Task", 4096, NULL, 1, NULL);
     xTaskCreate(uart_task, "uart_task", 4096, NULL, 1, NULL);
 
     vTaskStartScheduler();
